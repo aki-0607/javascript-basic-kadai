4919268bbd99872685b4d56d902667213cba4b6b
aki-0607 <sa.1203.wi@gmail.com>
2025-08-26 00:42:29 +0900

    JavaScript　課題008　提出

---
+++ kadai_007/calc.js
@@ -0,0 +1,18 @@
+let num = 30;
+
+console.log(num);
+
+switch (true) {
+  case num % 3 == 0&& num % 5 == 0:
+    console.log('3と5の倍数です')
+    break;
+  case num % 3 == 0:
+    console.log('3の倍数です')
+    break;
+  case num % 5 == 0:
+    console.log('5の倍数です')
+    break;
+  default:
+    console.log(num)
+    break;
+}
