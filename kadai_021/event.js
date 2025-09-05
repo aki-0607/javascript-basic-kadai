a12e30281d134b39dd71cfa12c9bfa01a303a538
aki-0607 <sa.1203.wi@gmail.com>
2025-09-05 16:58:45 +0900

    JavaScript　課題021　提出

---
+++ kadai_021/event.js
@@ -0,0 +1,13 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+// textというidを持つHTML要素を取得し、定数に代入する
+const text = document.getElementById('text');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+  setTimeout(() => {
+  // id="text"の文章を書き換えるには、textContent を使う。
+  text.textContent    = "ボタンをクリックしました。";
+  },2000);
+});
